49f77c9abb5ba40a7ecbbcff0812ed6c82bc3558
Jitender Jaiswal <53913030+JitenderJaiswal@users.noreply.github.com>
2020-01-17 10:58:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express=require('express');const app=express();
-const port=8000;    
-var bodyParser = require('body-parser');      
+const express=require('express');         //1.connect express server
+const app=express();
+const port=8000;   
 
-const db=require('./config/mongoose');
-app.use(bodyParser.urlencoded({ extended: false }));
+const db=require('./config/mongoose');    //2.connect mongodb
+
+var bodyParser = require('body-parser');  
+app.use(bodyParser.urlencoded({ extended: false }));//3.body parser middleware
      
-app.use(express.static('./assets'));
-app.set('layout extractStyles',true);//extract style and scripts from sub pages into the layout
+app.use(express.static('./assets'));      //4.static files html,css,javascript(frontend)
+app.set('layout extractStyles',true);    //extract style and scripts from sub pages into the layout
 app.set('layout extractScripts',true);
 
-app.use('/',require('./routes'));
-app.set('view engine','ejs');app.set('views','./views');
+app.use('/',require('./routes'));         //5.Router
+
+app.set('view engine','ejs');             //6.EJS(Embedded JavaScript templates)
+app.set('views','./views');
 
 app.listen(port,function(err){
-    if(err)console.log(`Error in running the server:${err}`);
+    if(err)
+    console.log(`Error in running the server:${err}`);
+     
     console.log(`Server is running on port:${port}`);
 });
