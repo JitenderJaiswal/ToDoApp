bf5477f8c216b40cb1aba21319868f0b58aedd43
Jitender Jaiswal <53913030+JitenderJaiswal@users.noreply.github.com>
2020-01-17 10:47:51 -0500

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,14 @@
 const todo=require('../models/todo');
 
 module.exports.create=async function(req,res){
-    try{ let Todo=await todo.create(req.query);
-        return res.redirect('back');
-      }catch(err){console.log('Error', err);return;}
+    try{ 
+        let Todo=await todo.create(req.query);
+                 return res.redirect('back');
+      }
+    catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
 
 module.exports.delete= async function(req,res){
@@ -14,7 +19,11 @@ module.exports.delete= async function(req,res){
          const id=checksid[i];
          await todo.deleteOne({_id:id}); 
        }
-     } catch(err){ console.log('Error', err); return;}
+     } 
+   catch(err){ 
+       console.log('Error', err); 
+       return;
+   }
 
    return res.redirect('back');
 }
