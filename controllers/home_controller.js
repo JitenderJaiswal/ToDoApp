6ac98053beea1ea0ae383e283eebb2bd01ece5dd
Jitender Jaiswal <53913030+JitenderJaiswal@users.noreply.github.com>
2020-01-17 10:49:04 -0500

    Update home_controller.js

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,9 +3,11 @@ const todo = require('../models/todo');
 module.exports.home = async function(req, res){
     try{
         let Todo=await todo.find({});
-        return res.render('home',{ title :"contactList",Todo:Todo});
-         
-       }catch(err){
+                 return res.render('home',{ title :"contactList",
+                                           Todo:Todo}
+                                  );
+       }
+    catch(err){
         console.log('Error', err);
         return;
     }
