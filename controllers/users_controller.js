8fb6926791bf8741b98c15af9540aafdc4e5b8aa
Jitender Jaiswal <53913030+JitenderJaiswal@users.noreply.github.com>
2020-09-18 06:29:11 -0400

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,16 @@ const todo=require('../models/todo');
 
 module.exports.create=async function(req,res){
     try{ 
-        
-        let Todo=await todo.create(req.query);
-                 return res.redirect('back');
+        const {desc,selects,date}=req.query;
+
+        if(!desc||!selects||!date){
+          req.flash('error','Please select all the fields!!!');
+          return res.redirect('back');
+        }
+
+        await todo.create(req.query);
+        req.flash('success','Task added successfully!!!');
+          return res.redirect('back');
       }
     catch(err){
         console.log('Error', err);
@@ -15,22 +22,20 @@ module.exports.create=async function(req,res){
 module.exports.delete= async function(req,res){
   try{
        const checksid=req.query.check;
-      
       if(Array.isArray(checksid)){
-      for(let i=1;i<checksid.length;i++){  
+      for(let i=0;i<checksid.length;i++){  
          const id=checksid[i];
-         console.log(id);
           await todo.deleteOne({_id:id}); 
        }
       }
       else{
         await todo.deleteOne({_id:checksid}); 
       }
+      req.flash('success','Task deleted successfully!!!');
+      return res.redirect('back');
      } 
    catch(err){ 
        console.log('Error', err); 
        return;
    }
-
-   return res.redirect('back');
 }
